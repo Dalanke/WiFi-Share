669dc0b97f89497cfba8f7248c8b0e54a79e1774
Dalanke <510934379@qq.com>
2017-01-23 11:29:49 +0800

    Add files via upload
    已知Win10 1607 下运行会出现bug 有可能引起系统自带的移动热点失效
    该问题是由网络共享设置引起的
    //功能说明：可自动完成网络共享设置，不必手动设置
                         添加连接人数显示
                         添加账号密码记忆功能

---
+++ WiFiShare/WiFiShare/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace WiFiShare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WiFi名称：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "密码：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(91, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "新建";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(23, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(220, 121);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "新建WiFi共享";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(276, 69);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(259, 88);
+            this.listBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(276, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "启动";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(360, 176);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "停止";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(441, 176);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "删除";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(46, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Location = new System.Drawing.Point(23, 149);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "共享设置";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "选择电脑连接到的网络";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(69, 71);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "确定";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "WiFi共享";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(242, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(293, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Tips：如果连接不上网络，请按删除后重新新建并启动";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 257);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(563, 22);
+            this.statusStrip1.TabIndex = 13;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(103, 17);
+            this.toolStripStatusLabel1.Text = "蹭网的人有：0 个";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 279);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "WiFiShare";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WiFiShare/WiFiShare/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using NETCONLib;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
+
+namespace WiFiShare
+{
+    public partial class Form1 : Form
+    {
+        string sc = "";
+        //string cs = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string CmdExecute(string command) //Cmd 执行
+        {
+            string output = ""; //输出字符串  
+            if (command != null && !command.Equals(""))
+            {
+                Process process = new Process();//创建进程对象  
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = @"c:\windows\system32\cmd.exe";//设定需要执行的命令  
+                startInfo.Arguments = "/C " + command;//“/C”表示执行完命令后马上退出  
+                startInfo.UseShellExecute = false;//不使用系统外壳程序启动  
+                startInfo.RedirectStandardInput = false;//不重定向输入  
+                startInfo.RedirectStandardOutput = true; //重定向输出  
+                startInfo.CreateNoWindow = true;//不创建窗口  
+                startInfo.Verb = "runas";
+                process.StartInfo = startInfo;
+
+                try
+                {
+                    if (process.Start())//开始进程  
+                    {
+                        output = process.StandardOutput.ReadToEnd();//读取进程的输出  
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    if (process != null)
+                        process.Close();
+                }
+            }
+            return output;
+        }
+
+        private void button1_Click(object sender, EventArgs e) //创建
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("账号密码不可以为空");
+                textBox1.Focus();
+            }
+            else if (textBox2.Text.Length < 8)
+                MessageBox.Show("密码长度不小于八位");
+            else
+            {
+                string command = "netsh wlan set hostednetwork mode=allow ssid=" + textBox1.Text + " key=" + textBox2.Text;
+                string str2 = CmdExecute(command);
+                MessageBox.Show(str2);
+                if (((str2.IndexOf("承载网络模式已设置为允许") > -1) && (str2.IndexOf("已成功更改承载网络的 SSID。") > -1)) && (str2.IndexOf("已成功更改托管网络的用户密钥密码。") > -1))
+                {
+                    listBox1.Items.Add(textBox1.Text + "   " + "新建共享网络成功  " + DateTime.Now.ToString("HH:mm:ss"));
+                }
+                else
+                {
+                    listBox1.Items.Add(textBox1.Text + "   " + "搭建失败请重试  " + DateTime.Now.ToString("HH:mm:ss"));
+                }
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)//禁止
+        {
+            string command = "netsh wlan set hostednetwork mode=disallow";
+            if (CmdExecute(command).IndexOf("承载网络模式已设置为禁止") > -1)
+            {
+                listBox1.Items.Add(textBox1.Text + "   " + "禁止共享网络成功！  " + DateTime.Now.ToString("HH:mm:ss"));
+            }
+            else
+            {
+                listBox1.Items.Add(textBox1.Text + "   " + "禁止失败！  " + DateTime.Now.ToString("HH:mm:ss"));
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)//启动
+        {
+            if (CmdExecute("netsh wlan start hostednetwork").IndexOf("已启动承载网络") > -1)
+            {
+                listBox1.Items.Add("启动成功！  " + DateTime.Now.ToString("HH:mm:ss"));
+            }
+            else
+            {
+                listBox1.Items.Add("启动失败，请尝试新建网络  " + DateTime.Now.ToString("HH:mm:ss"));
+            }
+            //读取所有网络
+            comboBox1.Items.Clear();
+            NetSharingManager manager = new NetSharingManager();
+            var connections = manager.EnumEveryConnection;
+            foreach (INetConnection c in connections)
+            {
+                var props = manager.NetConnectionProps[c];            
+                comboBox1.Items.Add(props.Name);
+            }
+            NetworkInterface[] Ninterface = NetworkInterface.GetAllNetworkInterfaces();//确定虚拟网络名称
+            foreach (NetworkInterface  IN in Ninterface)
+            {
+                if (IN.Description== "Microsoft Hosted Network Virtual Adapter")
+                {
+                    sc = IN.Name;
+                }
+            }
+            timer1.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (CmdExecute("netsh wlan stop hostednetwork").IndexOf("已停止承载网络") > -1)
+            {
+                listBox1.Items.Add("停止成功！  " + DateTime.Now.ToString("HH:mm:ss"));
+            }
+            else
+            {
+                listBox1.Items.Add("停止失败！  " + DateTime.Now.ToString("HH:mm:ss"));
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                CmdExecute("netsh wlan stop hostednetwork");
+                StreamWriter sw = new StreamWriter("Logs.txt", false);// textbox 记忆功能
+                sw.WriteLine(textBox1.Text);
+                sw.WriteLine(textBox2.Text);
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("应用程序退出但承载网络可能未停止");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)  //网络共享设置
+        {
+            if (comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("请先选择电脑连接到的网络！");
+            }
+            else
+            {
+                string connectionToShare = comboBox1.SelectedItem.ToString(); // 被共享的网络连接
+                string sharedForConnection = sc; // 共享的家庭网络连接
+
+                var manager = new NetSharingManager();
+                var connections = manager.EnumEveryConnection;
+
+                foreach (INetConnection c in connections)
+                {
+                    try
+                    {
+                        var props = manager.NetConnectionProps[c];
+                        var sharingCfg = manager.INetSharingConfigurationForINetConnection[c];
+                        if (props.Name == connectionToShare)
+                        {
+                            sharingCfg.EnableSharing(tagSHARINGCONNECTIONTYPE.ICSSHARINGTYPE_PUBLIC);
+                            if (sharingCfg.SharingEnabled == true)
+                                listBox1.Items.Add("已设置"+props.Name+"用于共享");
+                        }
+                        else if (props.Name == sharedForConnection)
+                        {
+                            sharingCfg.EnableSharing(tagSHARINGCONNECTIONTYPE.ICSSHARINGTYPE_PRIVATE);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("设置共享失败");
+                        throw;  
+                    }  
+                }
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                this.notifyIcon1.Visible = true;
+                notifyIcon1.BalloonTipText = "已经最小化到这里了哦";
+                notifyIcon1.ShowBalloonTip(5);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("Logs.txt"))
+            {
+                StreamReader sr = new StreamReader("Logs.txt");
+                textBox1.Text = sr.ReadLine();
+                textBox2.Text = sr.ReadLine();
+                sr.Close();
+            }  
+        }
+        private void Clients_Number()
+        {
+            string s="";
+            int n = CmdExecute("netsh wlan show hostednetwork").IndexOf("客户端数      :");
+            if(n>-1)
+            s=CmdExecute("netsh wlan show hostednetwork").Substring(n+11,2);
+            s = Regex.Replace(s, @"\D", "");
+            toolStripStatusLabel1.Text = "蹭网的人有："+s+"个";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Clients_Number();
+        }
+    }
+}
